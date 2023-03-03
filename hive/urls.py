d65841d1ba9b3fef4d9c9a87ca4e606d1699eaab
hadysoufan <hadisoufan74@gmail.com>
2023-03-03 17:02:05 +0200

    Importing include to let know the app urls

--- hive/urls.py
+++ hive/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def getHome(request):
-    return HttpResponse("Hello, world. You're at the home page.")
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', getHome, name='home')
-   
+    path('', include('projects.urls'))
 ]
