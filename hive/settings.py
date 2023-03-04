29c55b198bfd2a5c42281a306ee6167dd4bfd76c
hadysoufan <hadisoufan74@gmail.com>
2023-03-04 11:50:53 +0200

    Connecting users app with the hive app

--- hive/settings.py
+++ hive/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'projects.apps.ProjectsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -124,7 +125,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
-] 
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
