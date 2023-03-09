ede4afcd964a230d059f2cf166e3175ef1855e6e
hadysoufan <hadisoufan74@gmail.com>
2023-03-09 20:31:50 +0200

    Adding 'django_messages' to the MIDDLEWARE

--- hive/settings.py
+++ hive/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django_messages.middleware.MessagesMiddleware',
 ]
 
 ROOT_URLCONF = 'hive.urls'
