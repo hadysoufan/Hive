2771a56622a30496c9e69493437ae2f135fc0372
hadysoufan <hadisoufan74@gmail.com>
2023-03-10 09:29:21 +0200

    Editing

--- users/models.py
+++ users/models.py
@@ -1,22 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
-from django.contrib.auth import get_user_model
 
-
-User = get_user_model()
 
 # Create your models here.
 
 
 class Profile(models.Model):
-    _id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True)
     profileImg = models.ImageField(
-        upload_to='profiles/', default='profiles/default.png', null=True, blank=True)
+        upload_to='media', default='default.png')
     location = models.CharField(max_length=255, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = uuid.uuid4().hex
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return str(self.user.username)
