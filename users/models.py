984a0fb1eb803cb181a358264c3d6e049915d486
hadysoufan <hadisoufan74@gmail.com>
2023-03-10 16:54:05 +0200

    Adding Post model

--- users/models.py
+++ users/models.py
@@ -1,23 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
+from datetime import datetime
 
 # Create your models here.
 
 
 class Profile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=500, blank=True, null=True)
     username = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True)
     profileImg = models.ImageField(
-        upload_to='media', default='default.png')
+        upload_to='profileImg', default='default.png')
     location = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return str(self.user.username)
 
 
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='postImages')
+    caption = models.TextField()
+    createdAt = models.DateTimeField(default=datetime.now)
+    numOfLikes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.user
