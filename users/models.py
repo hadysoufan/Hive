40e72ed025c8eecddb631bab31e9c8d1d25ec44b
hadysoufan <hadisoufan74@gmail.com>
2023-03-10 14:17:09 +0200

    Moving signals to the signals.py file

--- users/models.py
+++ users/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
 import uuid
 
 # Create your models here.
@@ -22,23 +20,4 @@ class Profile(models.Model):
         return str(self.user.username)
 
 
-# @receiver(post_save, sender=Profile)
-def createProfile(sender, instance, created, **kwargs):
-    if created:
-        user = instance
-        profile = Profile.objects.create(
-            user=user,
-            username=user.username,
-            email=user.email,
-            name=user.first_name,
-        )
 
-
-# @receiver(post_delete, sender=Profile)
-def deleteProfile(sender, instance, **kwargs):
-    user = instance.user
-    user.delete()
-
-
-post_save.connect(createProfile, sender=User)
-post_delete.connect(deleteProfile, sender=Profile)
