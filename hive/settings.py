b515acc89cbee5fba83a9c3b2c10adf8d3080f89
hadysoufan <hadisoufan74@gmail.com>
2023-03-11 16:06:44 +0200

    Editing

--- hive/settings.py
+++ hive/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
 ]
 
 ROOT_URLCONF = 'hive.urls'
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
@@ -131,6 +134,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOTS = os.path.join(BASE_DIR, 'media')
--- hive/urls.py
+++ hive/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -7,4 +9,5 @@ urlpatterns = [
     path('', include('projects.urls')),
     path('users/', include('users.urls')),
     path('hive/', include('hivePlatform.urls'))
-]
+] + static(settings.MEDIA_URL,
+           document_root=settings.MEDIA_ROOT)
