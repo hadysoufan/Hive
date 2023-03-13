a477f1e6308d88d57f0f277366f19c762c048c60
hadysoufan <hadisoufan74@gmail.com>
2023-03-13 20:09:54 +0200

    Editing

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from . models import Profile, Post
+from django.contrib.auth.models import User
+
 
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Post)
+
+
--- users/signals.py
+++ users/signals.py
@@ -9,26 +9,48 @@ profile_updated = Signal()
 
 # @receiver(post_save, sender=Profile)
 def createProfile(sender, instance, created, **kwargs):
-    print('Profile Signal Triggered')
     if created:
+        print('Profile Signal Triggered')
         user = instance
         profile = Profile.objects.create(
             user=user,
             username=user.username,
-            email=user.email,
             name=user.first_name,
+            email=user.email,
         )
+        profile.save()
+        print(f'username: {user.username}')
+        print(f'name: {user.first_name}')
+        print(f'email: {user.email}')
 
 
-@receiver(post_save, sender=Profile)
+# @receiver(post_save, sender=Profile)
 def updateUser(sender, instance, created, **kwargs):
     profile = instance
     user = profile.user
     if created == False:
+        print('created user triggered...')
         user.first_name = profile.name
         user.username = profile.username
         user.email = profile.email
         user.save()
+        print(f'user name: {user.first_name}')
+        print(f'user username: {user.username}')
+        print(f'user email: {user.email}')
+
+    # Check if user exists in the admin panel
+    if User.objects.filter(id=user.id).exists():
+        print('existed user triggered...')
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.bio = profile.bio
+        user.profileImg = profile.profileImg
+        user.save()
+        print(f'Profile bio: {profile.bio}')
+        print(f'User bio: {user.bio}')
+        print(f'User id:{user.id}')
+        print(f'user profileImg: {user.profileImg}')
 
         # Send signal to update HTML form
         profile_updated.send(sender=Profile, user=user)
